b92410f0e55584cd3d60b64fb41cc0022e3850b5
anargu <anthony.arostegui@gmail.com>
2022-02-17 19:31:48 -0500

    updated GameMenu wip

--- src/components/GameMenu.js
+++ src/components/GameMenu.js
@@ -1,12 +1,26 @@
-import React from "reactn";
+import React, { useState } from "reactn";
 import styled from "styled-components";
 import {Collapse} from "antd";
-import {ButtonAnt, Select} from "./form";
+import {ButtonAnt, Select, Switch, Input} from "./form";
 import {languages} from "./common/DataList";
+import defaultTo from "lodash/defaultTo";
+import { config, firestore } from "../firebase";
+import { Image } from "./common/Image";
 
 const { Panel } = Collapse;
 
 export const GameMenu = (props) => {
+
+  const [awards, setAwards] = useState([
+    {
+      name: "",
+      order: 1,
+    },
+  ]);
+  const [showAwards, setShowAwards] = useState(true);
+
+  const [userIdentity, setUserIdentity] = useState(false);
+
   return (
     <GameCss className="w-full m-auto max-w-[500px] p-4 text-white md: p-[20px]">
       <div>
@@ -16,9 +30,9 @@ export const GameMenu = (props) => {
 
         {props.showChooseGameMode && (
           <div className="grid grid-cols-1	gap-[5px] p-[5px] pt-[10px]">
-            <div className="font-['Lato'] p-[15px] text-[15px] leading-8 rounded-[4px] items-center bg-primary">
-              <div className="w-full text-center">Versión Simple</div>
-              <div className="w-full text-center">La manera sencilla</div>
+            <div className="font-['Lato'] p-[15px] text-[15px] font-bold leading-8 rounded-[4px] items-center bg-primary">
+              <div className="w-full text-center">Jugadores vs Jugadores</div>
+              <div className="w-full text-center mb-2">1:1 dispositivos</div>
               <ButtonAnt
                 className="font-bold"
                 color="success"
@@ -27,7 +41,7 @@ export const GameMenu = (props) => {
                 disabled={props.isLoadingSave}
                 onClick={() => props.createLobby("individual")}
               >
-                Jugar
+                Clásico
               </ButtonAnt>
             </div>
           </div>
@@ -36,6 +50,21 @@ export const GameMenu = (props) => {
         <Collapse defaultActiveKey={["1"]} accordion className="border-none">
           <Panel header="Opciones del juego" key="1">
             <div className="options">
+
+              <div class="mb-3 py-3 text-center font-bold bg-whiteLight rounded-[2px] shadow-[0px_4px_0px_rgb(196,196,196)]">Recomendado</div>
+
+              <div className="grid grid-cols-[6fr_2fr] items-center mx-auto my-[2px] px-[10px] py-[5px] text-[13px] leading-[16px] bg-primaryDarken rounded-[2px] text-whiteLight">
+                <div>
+                  <div className="font-bold">Identificador de jugador</div>
+                  <span>Conoce el nombre de la persona atrás del nickname</span>
+                </div>
+                <div className="justify-self-end mt-4">
+                  <Switch defaultChecked={userIdentity} onChange={() => setUserIdentity(!userIdentity)} />
+                </div>
+              </div>
+
+              <div class="mt-3 mb-3 py-3 text-center font-bold bg-whiteLight rounded-[2px] shadow-[0px_4px_0px_rgb(196,196,196)]">General</div>
+
               <div className="grid grid-cols-[5fr_3fr] items-center mx-auto my-[2px] px-[10px] py-[5px] text-[13px] leading-[16px] bg-primaryDarken rounded-[2px] text-whiteLight">
                 <div>
                   <div className="font-bold">Idioma</div>
@@ -51,6 +80,7 @@ export const GameMenu = (props) => {
                   onChange={(value) => props.onLanguageChange?.(value)}
                 />
               </div>
+
               <div className="grid grid-cols-[5fr_3fr] items-center mx-auto my-[2px] px-[10px] py-[5px] text-[13px] leading-[16px] bg-primaryDarken rounded-[2px] text-whiteLight">
                 <div>
                   <div className="font-bold">Música en el Lobby</div>
@@ -66,6 +96,71 @@ export const GameMenu = (props) => {
                   onChange={(value) => props.onAudioChange?.(value)}
                 />
               </div>
+
+              <div className="grid grid-cols-[5fr_3fr] items-center mx-auto my-[2px] px-[10px] py-[5px] text-[13px] leading-[16px] bg-primaryDarken rounded-[2px] text-whiteLight">
+                <div>
+                  <div className="font-bold">Premio</div>
+                </div>
+                <div className="justify-self-end mt-4">
+                  <Switch defaultChecked={showAwards} onChange={() => setShowAwards(!showAwards)} />
+                </div>
+              </div>
+
+              {showAwards && (
+                <div id={awards.length} className="grid items-center mx-auto my-[2px] px-[10px] py-[5px] text-[13px] leading-[16px] bg-primaryDarken rounded-[2px] text-whiteLight">
+                  {defaultTo(awards, []).map((award, index) => (
+                    <div className="grid grid-cols-[auto_min-content] my-1" key={`award-${index}`}>
+                      <Input
+                        type="text"
+                        name={`award-${index}`}
+                        defaultValue={award.name}
+                        onBlur={(e) => {
+                          let newAwards = awards;
+                          newAwards[index].name = e.target.value;
+                          setAwards([...newAwards]);
+                        }}
+                        placeholder={`Premio ${index + 1}`}
+                        className={"dark"}
+                        key={`award-${index}-${award.order}`}
+                      />
+                      <button
+                        className="float-right px-2"
+                        onClick={() => {
+                          let newAwards = awards.filter((award, idx) => idx !== index);
+
+                          setAwards([...newAwards]);
+                        }}
+                      >
+                        <Image
+                          src={`${config.storageUrl}/resources/close.svg`}
+                          height="15px"
+                          width="15px"
+                          cursor="pointer"
+                          size="contain"
+                          margin="0"
+                        />
+                      </button>
+                    </div>
+                  ))}
+                  <ButtonAnt
+                    color="secondary"
+                    margin="0.5rem 0 0.5rem auto"
+                    onClick={() => {
+                      setAwards([
+                        ...awards,
+                        {
+                          name: "",
+                          id: firestore.collection("awards").doc().id,
+                          order: awards.length + 1,
+                        },
+                      ]);
+                    }}
+                  >
+                    Agregar
+                  </ButtonAnt>
+                </div>
+              )}
+
             </div>
           </Panel>
         </Collapse>
