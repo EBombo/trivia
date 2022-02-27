688d5a5b2c623c1c0eee50118dc40078951f6b75
anargu <anargu@pop-os.localdomain>
2022-02-26 20:43:38 -0500

    wip result answer

--- src/pages/lobbies/_lobbyId/play/LobbyInPlay.js
+++ src/pages/lobbies/_lobbyId/play/LobbyInPlay.js
@@ -21,6 +21,7 @@ const TRUE_OR_FALSE = 'truefalse';
 const OPEN = 'open';
 
 const LOADING_STATE = 'LOADING';
+const QUESTION_RESULT_STATE = 'QUESTION_RESULT';
 
 export const LobbyInPlay = (props) => {
   const router = useRouter();
@@ -50,6 +51,27 @@ export const LobbyInPlay = (props) => {
         </div>
         <div class="font-bold text-whiteLight text-xl">¿Te sientes confiado?</div>
       </div>);
+
+  if (props.lobby.game?.state !== QUESTION_RESULT_STATE)
+    return (
+      <div className="font-['Lato'] font-bold bg-secondary bg-center bg-contain bg-lobby-pattern w-screen min-h-screen overflow-auto text-center">
+        <div className="min-h-screen flex flex-col justify-center bg-secondaryDark bg-opacity-50">
+          <div className="relative my-4 mx-4 pt-8 pb-4 bg-whiteLight text-lg min-w-[300px] self-center rounded-lg">
+            <div className="absolute top-[-20px] left-1/2 translate-x-[-50%] bg-success rounded max-w-[250px] whitespace-nowrap px-8 text-secondaryDarken">
+              <span className="inline-block py-4 pr-4 align-middle">
+                <Image src={`${config.storageUrl}/resources/check-with-depth.svg`} width="16px"/>
+              </span>
+              Respuesta correcta
+            </div>
+
+            <div className="text-black"><span className="inline-block py-4 align-middle"><Image src={`${config.storageUrl}/resources/red-fire-streak.svg`} width="12px"/></span> Racha de respuestas: 8</div>
+            <div className="text-black text-3xl py-8">+600 puntos</div>
+            <div className="text-black">Puntaje actual: 1500 pts</div>
+            <div className="text-black">Puesto: 25/200</div>
+          </div>
+        </div>
+      </div>
+    );
 
   return (
     <div className="font-['Lato'] font-bold bg-secondary w-screen min-h-screen bg-center bg-contain bg-lobby-pattern overflow-auto">
