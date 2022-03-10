dd7a4664666c7090bb4f2023fe5378eb13c80414
anargu <anargu@pop-os.localdomain>
2022-03-10 17:02:09 -0500

    integrate logic

--- src/pages/lobbies/_lobbyId/play/BarResult.js
+++ src/pages/lobbies/_lobbyId/play/BarResult.js
@@ -24,7 +24,7 @@ export const BarResult = (props) => {
         <Image src={getIconUrl(props.color)} width="24px" />
       </div>
       <div className={`relative h-[50px] ${colorClass} rounded`} style={{ width: `${props.value ?? 1}%` }}>
-        {true && (
+        {props.isCorrect && (
           <div className="absolute right-4 top-4 pl-2 text-2xl font-bold">
             <Image src={`${config.storageUrl}/resources/check.svg`} width="24px" />
           </div>
