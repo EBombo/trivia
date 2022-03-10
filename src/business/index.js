ba052392bd57e97453d83e3e8a7720158c502e38
anargu <anargu@pop-os.localdomain>
2022-03-10 13:53:52 -0500

    fix scoring logic

--- src/business/index.js
+++ src/business/index.js
@@ -14,3 +14,8 @@ export const SPEED = {
 
 export const getCurrentQuestion = (questions, currentQuestionNumber) =>
   find(questions, (q) => q.questionNumber === currentQuestionNumber);
+
+// kahoot scoring [1-(r/q/2)]p
+// r = time left, q = total time, p = points
+export const computePointsEarned = (timeLeft, totalTime, point) =>
+  (1 - ((totalTime - timeLeft) / totalTime / 2)) * point;
