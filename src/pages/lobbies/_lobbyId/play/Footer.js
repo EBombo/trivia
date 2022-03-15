09e0264e2a76bfaaa8818ec611a0013c7de33c06
anargu <anargu@pop-os.localdomain>
2022-03-15 17:31:44 -0500

    footer updated

--- src/pages/lobbies/_lobbyId/play/Footer.js
+++ src/pages/lobbies/_lobbyId/play/Footer.js
@@ -1,13 +1,13 @@
 import React from "reactn";
-import { config, hostName } from "../../../../firebase";
+import { hostName } from "../../../../firebase";
 
 export const Footer = (props) => (
-  <div className="text-whiteLight flex flex-col md:flex-row md:justify-between items-center mx-4 py-4">
-    <div className="text-lg md:text-2xl px-4 py-2">
+  <div className="text-whiteLight flex flex-col md:flex-row md:justify-between items-center mx-4 min-h-[50px]">
+    <div className="text-lg md:text-xl px-4 py-2">
       <span>Entra a </span>
       <span className="font-bold">{hostName}</span>
     </div>
-    <div className="text-2xl px-4 py-2">
+    <div className="text-xl px-4 py-2">
       <span>PIN </span>
       <span>{props.lobby?.pin}</span>
     </div>
