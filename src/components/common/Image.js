8914bc1990c84302cbfe2ad379fde01e23ca1302
anargu <anargu@pop-os.localdomain>
2022-03-16 13:12:11 -0500

    added alt property for seo

--- src/components/common/Image.js
+++ src/components/common/Image.js
@@ -5,7 +5,7 @@ import { mediaQuery } from "../../constants";
 export const Image = (props) => (
   <ImageCss {...props} ref={props.innerRef}>
     {!props.noImgTag && (
-      <img src={props.src} alt={props.src} />
+      <img src={props.src} alt={props.alt || props.src} />
     )}
   </ImageCss>
 );
