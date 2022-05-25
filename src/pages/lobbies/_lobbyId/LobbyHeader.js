d381187771df9980a786d4e1169b6bc435786d5c
anargu <anargu@pop-os.localdomain>
2022-05-25 00:31:49 -0500

    set merge when updating user

--- src/pages/lobbies/_lobbyId/LobbyHeader.js
+++ src/pages/lobbies/_lobbyId/LobbyHeader.js
@@ -96,7 +96,7 @@ export const LobbyHeader = (props) => {
             .doc(lobbyId)
             .collection("users")
             .doc(user.id)
-            .set({ ...user, option: user.username })
+            .set({ ...user, option: user.username }, { merge: true })
       );
 
       await Promise.all(promisesUsers);
