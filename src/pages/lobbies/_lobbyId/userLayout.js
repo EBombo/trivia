2255a290bc400709fdd19c9cab6ba8fbcc366e24
Pablo <pablo.yncio@immertec.com>
2022-07-15 17:04:00 -0500

    fix countdown

--- src/pages/lobbies/_lobbyId/userLayout.js
+++ src/pages/lobbies/_lobbyId/userLayout.js
@@ -2,7 +2,7 @@ import React, { useGlobal, useState } from "reactn";
 import styled from "styled-components";
 import { Popover, Slider, Spin, Tooltip } from "antd";
 import { Desktop, mediaQuery } from "../../../constants";
-import { config, firestore, firestoreBomboGames, hostName } from "../../../firebase";
+import { config, firebase, firestore, firestoreBomboGames, hostName } from "../../../firebase";
 import { Image } from "../../../components/common/Image";
 import { LoadingOutlined, MessageOutlined } from "@ant-design/icons";
 import { ButtonAnt } from "../../../components/form";
@@ -209,16 +209,18 @@ export const UserLayout = (props) => {
                       return await props.logout();
                     }
 
-                    let promiseUser = null;
+                    if (!props.lobby?.isPlaying) return await props.logout();
 
-                    if (props.lobby?.isPlaying) {
-                      promiseUser = firestore
-                        .collection("lobbies")
-                        .doc(props.lobby.id)
-                        .collection("users")
-                        .doc(authUser.id)
-                        .update({ hasExited: true });
-                    }
+                    const promiseUser = firestore
+                      .collection("lobbies")
+                      .doc(props.lobby.id)
+                      .collection("users")
+                      .doc(authUser.id)
+                      .update({ hasExited: true });
+
+                    const promiseLobby = firestore.doc(`lobbies/${props.lobby.id}`).update({
+                      countPlayers: firebase.firestore.FieldValue.increment(-1),
+                    });
 
                     await Promise.all([promiseUser, promiseLobby]);
 
